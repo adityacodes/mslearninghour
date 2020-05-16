674f9ff677b7419ddefedce5290e54b9fc10831e
adityacodes <adityapadhi1996@gmail.com>
2020-05-17 00:38:34 +0300

    Final Commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug')
+app.use(express.static('public'))
 
 const port = process.env.PORT || 1337;
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello Aditya!' })
+    res.render('index', { title: 'ExpressJS WebApp with Azure Cloud', message: 'Hello Aditya!' })
 });
 
 
-app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`); });
+app.listen(port, () => { console.log(`App listening at http://localhost:${port}`); });
