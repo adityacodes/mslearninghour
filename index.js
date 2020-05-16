8723878d163352995b618985efd8ba8df08c39ef
adityacodes <adityapadhi1996@gmail.com>
2020-05-16 20:22:24 +0300

    Added Express, Body Parser and Pug

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug')
 
 const port = process.env.PORT || 1337;
 
-app.get('/', (req, res) => res.send('Hello Adi!'))
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello Aditya!' })
+});
+
 
 app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`); });
